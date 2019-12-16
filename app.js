414decc640a0f148341df59f3599042f5af20238
guohaomeng <3020721699@qq.com>
2019-12-16 20:09:39 +0800

    111

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
-var http = require('http');
-var querystring = require('querystring');
-var util = require('util');
-var url = require('url');
-const cors=require('cors');//解决跨域
-
+var createError = require('http-errors');
 var express = require('express');
-var fs = require("fs");
-var app = module.exports = express();
-//配置
-function configure() {
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('keyboard cat'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-}
-//configure();
-//保存base64图片POST方法
-app.post('/upload', function (req, res) {
-  //接收前台POST过来的base64
-  var imgData = req.body.imgData;
-  //过滤data:URL
-  var base64Data = imgData.replace(/^data:image\/\w+;base64,/, "");
-  var dataBuffer = new Buffer(base64Data, 'base64');
-  fs.writeFile("out.png", dataBuffer, function (err) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send("保存成功！");
-    }
-  });
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
-if (!module.parent) {
-  app.listen(8082);
-  console.log('Express started on port 8082');
-}
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,35 +1,52 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
- 
- 
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+
+app.use('/public', express.static('public'));
+
+app.get('/index.html', function (req, res) {
+  res.sendFile(__dirname + "/" + "index.html");
+})
+
+//保存base64图片POST方法
+app.post('/upload', function (req, res) {
+  //接收前台POST过来的base64
+  var imgData = req.body.imgData;
+  //过滤data:URL
+  var base64Data = imgData.replace(/^data:image\/\w+;base64,/, "");
+  var dataBuffer = new Buffer(base64Data, 'base64');
+  fs.writeFile(gettime(), dataBuffer, function (err) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send("保存成功！");
+    }
+  });
+});
 // 创建服务器
-http.createServer( function (request, response) {  
-   // 解析请求，包括文件名
-   var pathname = url.parse(request.url).pathname;
-   
-   // 输出请求的文件名
-   console.log("Request for " + pathname + " received.");
-   
-   // 从文件系统中读取请求的文件内容
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP 状态码: 404 : NOT FOUND
-         // Content Type: text/html
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else{             
-         // HTTP 状态码: 200 : OK
-         // Content Type: text/html
-         response.writeHead(200, {'Content-Type': 'text/html'});    
-         
-         // 响应文件内容
-         response.write(data.toString());        
-      }
-      //  发送响应数据
-      response.end();
-   });   
-}).listen(8081);
- 
-// 控制台会输出以下信息
-console.log('Server running at http://localhost:8081/');
+var server = app.listen(8081, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("访问地址为 http://%s:%s" + "/public/index.html", host, port);
+
+})
+//获取时间
+function gettime() {
+  var date = new Date();
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+  //console.log(year+'年'+month+'月'+day+'日 '+hour+':'+minute+':'+second);
+  var time = year + '' + month + '' + day + '' + hour + '' + minute + '' + second;
+  console.log(time);
+  var filename = time + ".png"
+  return filename;
+}
